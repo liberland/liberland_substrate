6c80bbcabdd2fd3fad2f1db756db0f31335635cd
Max Gravitt <max@digitalscarcity.io>
2022-03-17 17:15:56 -0400

    added testnet.json

--- scripts/session_keys/insert.sh
+++ scripts/session_keys/insert.sh
@@ -1,4 +1,4 @@
-curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@babe4"
-curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@gran4"
-curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@imol4"
-curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@audi4"
+curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@babe3"
+curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@gran3"
+curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@imol3"
+curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@audi3"
