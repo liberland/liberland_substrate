d59a729003f81fa1021b43f5e1a14b8ef652f94b
Filip <flipchan@riseup.net>
2022-05-13 12:15:51 +0400

    transaction balance working

--- frame/llm-pallet/src/lib.rs
+++ frame/llm-pallet/src/lib.rs
@@ -26,17 +26,17 @@ pub mod pallet {
 		PalletId,
 	};
 	use frame_system::{ensure_signed, pallet_prelude::*};
-	use sp_runtime::{generic::BlockId, SaturatedConversion};
+	use sp_runtime::{generic::BlockId, traits::StaticLookup, SaturatedConversion};
 	use sp_std::vec::Vec;
-	use sp_runtime::traits::StaticLookup;
 	// Every year the pallet mints 0.9% of the total supply.
 	//	use frame_support::traits::tokens::AssetId;
 	//pub type AssetId = u64;
 	use frame_system::Origin;
+	use sp_runtime::traits::Saturating;
 	//use sp_runtime::traits::{AtLeast32Bit, MaybeSerializeDeserialize, Member, StaticLookup,
 	// Zero};
 
-//	type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+	//	type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
 	//	type AssetsWrapper: GetAsset<<Self as pallet_assets::Config>::AssetId>;
 	//	type BalanceOf<T> = <<T as Config>::Currency as Currency<AccountIdOf<T>>>::Balance;
 	//pub type AssetId = u64;//<T as pallet_assets::Config>::AssetId + u64; , u64 might to big?
@@ -58,38 +58,33 @@ pub mod pallet {
 	}
 	//	pub const CORE_ASSET_ID: AssetId = 0;
 
-// asset querys
+	// asset querys
 	//	pub fn get_asset_meta_data(asset_id: AssetId) -> Option<MetaData<AssetId>> {
-/*
-		#[pallet::storage]
-	#[pallet::getter(fn asset_store)]
-	pub(super) type AssetStore<T: Config> = StorageMap<_, 
-	Blake2_128Concat,
-	u32, 
-	AssetInfo, 
-	ValueQuery>; // asset id, asset meta data
-*/
-
+	/*
+			#[pallet::storage]
+		#[pallet::getter(fn asset_store)]
+		pub(super) type AssetStore<T: Config> = StorageMap<_,
+		Blake2_128Concat,
+		u32,
+		AssetInfo,
+		ValueQuery>; // asset id, asset meta data
+	*/
 
 	#[pallet::storage]
 	#[pallet::getter(fn get_balance)]
-	pub(super) type LLMBalance<T: Config> = StorageMap<
-		_,
-		Blake2_128Concat,
-		T::AccountId,
-		T::Balance,
-		ValueQuery
-		>;
+	pub(super) type LLMBalance<T: Config> =
+		StorageMap<_, Blake2_128Concat, T::AccountId, T::Balance, ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn minted_amount)]
 	/// Keep track of the amount of minted llm
 	pub(super) type MintedAmount<T: Config> = StorageValue<_, u64, ValueQuery>; // ValueQuery ,  OnEmpty = 0u64
 
-// Guess we dont need this type of stuff since we use pallet-assets to keep track of things
-//	#[pallet::storage]
-//	#[pallet::getter(fn llm_balance_sheet)]
-//	pub(super) type llm_balance_sheet<T: Config> = StorageMap<_, T::AccountId, u64, ValueQuery>; // ValueQuery ,  OnEmpty = 0u64
+	// Guess we dont need this type of stuff since we use pallet-assets to keep track of things
+	//	#[pallet::storage]
+	//	#[pallet::getter(fn llm_balance_sheet)]
+	//	pub(super) type llm_balance_sheet<T: Config> = StorageMap<_, T::AccountId, u64, ValueQuery>;
+	// // ValueQuery ,  OnEmpty = 0u64
 
 	//#[pallet::storage]
 	//#[pallet::getter(fn meta_data)]
@@ -97,9 +92,9 @@ pub mod pallet {
 	//pub(super) type MetaData<T: Config> = StorageValue<_, MetaData<T::Balance, BlockId>,
 	// ValueQuery>;
 
-//	pub trait GetAsset<AssetId> {
-//		fn id(asset: &AssetId) -> Option<AssetId>;
-//	}
+	//	pub trait GetAsset<AssetId> {
+	//		fn id(asset: &AssetId) -> Option<AssetId>;
+	//	}
 
 	#[pallet::config]
 	pub trait Config: pallet_assets::Config + frame_system::Config {
@@ -117,7 +112,7 @@ pub mod pallet {
 			+ From<u32>
 			+ Ord
 			+ Copy;
-	//	type AccountId: Parameter + Member + Default + Copy; //AtLeast32Bit
+		//	type AccountId: Parameter + Member + Default + Copy; //AtLeast32Bit
 		//type AssetId: IsType<<Self as pallet_assets::Config>::AssetId> + Parameter + Ord + Copy +
 		// Into<u64> + From<u64> + Default; //GetAsset<<Self as pallet_assets::Config>::AssetId> +
 		// type AssetId: IsType<<Self as pallet_assets::Config>::AssetId> + From<u64> + From<i32>;
@@ -139,6 +134,9 @@ pub mod pallet {
 		MaxCap,
 		/// Asset already created
 		AssetExists,
+		InvalidAccount,
+		// can not transfer asset
+		InvalidAssetMove,
 	}
 
 	#[pallet::pallet]
@@ -150,9 +148,9 @@ pub mod pallet {
 		fn on_initialize(_n: T::BlockNumber) -> Weight {
 			// convert blocknumber to u64
 			//println!("LLM PAllet");
-			
-		//	let blocknumber = blocknumber.saturated_into::<u64>();
-		//	Self::try_mint(blocknumber).unwrap_or_default();
+
+			//	let blocknumber = blocknumber.saturated_into::<u64>();
+			//	Self::try_mint(blocknumber).unwrap_or_default();
 			// todo: have a function that runs on blocknumber as input and checks if it can mint llm
 			// based on blocknumber 	Self::mint_llm(origin);
 			0
@@ -165,6 +163,24 @@ pub mod pallet {
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+		#[pallet::weight(10_000)] // debug function, used for testing,
+		pub fn fake_send(
+			origin: OriginFor<T>,
+			to_account: T::AccountId,
+			amount: T::Balance,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			let receiver = to_account.clone();
+			LLMBalance::<T>::insert::<T::AccountId, T::Balance>(
+				receiver,
+				amount.try_into().unwrap_or_default(),
+			);
+
+			pallet_assets::Pallet::<T>::mint_into(Self::llm_id().into(), &to_account, amount)
+				.unwrap_or_default();
+			Ok(())
+		}
+
 		//type AssetId = T::AssetId;
 		#[pallet::weight(10_000)] // change me
 		pub fn send_llm(
@@ -172,30 +188,74 @@ pub mod pallet {
 			to_account: T::AccountId,
 			amount: u64,
 		) -> DispatchResult {
-			let sender = ensure_signed(origin)?;
+			let sender = ensure_signed(origin.clone())?;
+			let lookup_account = T::Lookup::unlookup(to_account.clone());
+
 			let receiver = to_account;
-	//		pallet_assets::Pallet::<T>::transfer(
-	//			origin, 
-	//			llm_id,
-	//			lookup_account,
-	//			amount.into()
-	//		)
+
+			let amount_balance: T::Balance =
+				amount.try_into().map_err(|_| Error::<T>::InvalidAmount)?;
+			let sender_balance: T::Balance = LLMBalance::<T>::get(&sender) - amount_balance;
+			pallet_assets::Pallet::<T>::transfer(
+				origin.clone(),
+				Self::llm_id().into(),
+				lookup_account,
+				amount_balance.clone(),
+			)
+			.unwrap_or_default();
+
+			//	LLMBalance::<T>::mutate(sender, |balance| *balance =
+			// balance.saturating_sub(amount_balance));
+			LLMBalance::<T>::mutate_exists(&sender, |b| *b = Some(sender_balance));
+			let receiver_balance: T::Balance = LLMBalance::<T>::get(&receiver) + amount_balance;
+
+			LLMBalance::<T>::insert::<T::AccountId, T::Balance>(receiver.clone(), receiver_balance); // overwrite each time, its a bit computational heavy, this should be replaced with a
+																						 // match statement
+																						 /*
+																							 LLMBalance::<T>::mutate(&sender, |balance| -> Result<(), Error<T>> {
+																									 *balance = amount_balance;//balance
+																										 //.saturating_sub(amount_balance);
+																										 //.ok_or(Error::<T>::InvalidAmount)?;
+																									 Ok(())
+																								 }).map_err(|_| Error::<T>::InvalidAccount)?;
+																						 */
+
+			Event::<T>::TransferedLLM(sender, receiver, amount);
+
+			/*
+				match LLMBalance::<T>::get(&receiver) {
+					Some(balance) => {
+						LLMBalance::<T>::mutate(receiver, |balance| *balance = balance.saturating_add(amount_balance));
+						Ok(())
+					}
+					None => {
+						LLMBalance::<T>::insert::<T::AccountId, T::Balance>(receiver, amount.try_into().unwrap_or_default());
+						return Ok(());
+					},
+				}
+			*/
+
 			//	let ai: T::AssetId = CORE_ASSET_ID;
 			// Check account balance of llm for sender
 			//ensure!(<pallet_assets::Pallet<T>>::balance(ai, &sender.into()) >= amount,
 			// Error::<T>::LowBalance);// ensure balance is more or the same as the amount
 			// 	<pallet_assets::Pallet<T>>:: force_transfer(origin, ai, sender, receiver, amount)?;
 			// // transfer llm
-		//	<BalanceToAccount<T>>::insert(&to_account, amount);
-			todo!("Transfer llm");
+			//	<BalanceToAccount<T>>::insert(&to_account, amount);
+			//	todo!("Transfer llm");
+			Ok(())
 		}
 
-		#[pallet::weight(10_000)] 
-		pub fn send_locked_llm(origin: OriginFor<T>, amount: u64, to_account: T::AccountId) -> DispatchResult {
-				todo!("send_locked_llm");
+		#[pallet::weight(10_000)]
+		pub fn send_locked_llm(
+			origin: OriginFor<T>,
+			amount: u64,
+			to_account: T::AccountId,
+		) -> DispatchResult {
+			todo!("send_locked_llm");
 		}
 
-		/// Lock in LLM for 
+		/// Lock in LLM for
 		#[pallet::weight(10_000)] // change me
 		pub fn lock_llm(origin: OriginFor<T>, amount: u64) -> DispatchResult {
 			todo!("lock_llm");
@@ -204,58 +264,74 @@ pub mod pallet {
 
 		#[pallet::weight(10_000)] // change me
 		pub fn unlock_llm(origin: OriginFor<T>) -> DispatchResult {
-				todo!("unlock_llm"); // thaw
+			todo!("unlock_llm"); // thaw
 		}
 
 		#[pallet::weight(10_000)] // change me
 		pub fn createllm(origin: OriginFor<T>) -> DispatchResult {
-		//	T::AddOrigin::ensure_origin(origin)?;
+			//	T::AddOrigin::ensure_origin(origin)?;
 			let sender = ensure_signed(origin.clone())?;
 			Self::create_llm(origin)?;
-			
+
 			Ok(())
 		}
 
-
 		#[pallet::weight(10_000)] // change me
-		pub fn delegated_transfer(origin: OriginFor<T>, to_account: T::AccountId, amount: u64) -> DispatchResult {
-// the senate must approve this transfer
+		pub fn delegated_transfer(
+			origin: OriginFor<T>,
+			to_account: T::AccountId,
+			amount: u64,
+		) -> DispatchResult {
+			// the senate must approve this transfer
 			let sender = ensure_signed(origin)?;
 			let receiver = to_account;
-		//	pallet_assets::Pallet::<T>::delegated_transfer(
-		//		origin,
-		//		assetid,
-		//		delegate_account,
+			//	pallet_assets::Pallet::<T>::delegated_transfer(
+			//		origin,
+			//		assetid,
+			//		delegate_account,
 
-		//	)
-				todo!("delegated_transfer");
+			//	)
+			todo!("delegated_transfer");
+		}
+
+		#[pallet::weight(10_000)] // change me, remove me in prod
+		pub fn add_balance(origin: OriginFor<T>, amount: u128) -> DispatchResult {
+			let sender: T::AccountId = ensure_signed(origin)?;
+			//	let my_account: T::AccountId = origin.ac
+			todo!("added checks to verify and asset transfer and balance changes mutate");
+			Self::load_alice(amount, sender);
+			Ok(())
 		}
 
 		/// Allow the senate to approve transfers
-		#[pallet::weight(10_000)] 
-		pub fn approve_transfer(origin: OriginFor<T>, to_account: T::AccountId, amount: u64) -> DispatchResult {
-				todo!("approve_transfer");
+		#[pallet::weight(10_000)]
+		pub fn approve_transfer(
+			origin: OriginFor<T>,
+			to_account: T::AccountId,
+			amount: u64,
+		) -> DispatchResult {
+			todo!("approve_transfer");
 		}
 
 		/// Display the minted amount of llm
-//		#[pallet::weight(10_000)] // change me
-//		pub fn llm_circulation(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
-//			let sender = ensure_signed(origin)?;
-//			let minted_amount: u64 = <MintedAmount<T>>::get();
-//			Ok(())
-//		}
+		//		#[pallet::weight(10_000)] // change me
+		//		pub fn llm_circulation(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
+		//			let sender = ensure_signed(origin)?;
+		//			let minted_amount: u64 = <MintedAmount<T>>::get();
+		//			Ok(())
+		//		}
 
 		#[pallet::weight(10_000)]
 		pub fn mint_llm(origin: OriginFor<T>) -> DispatchResult
 //	where <T as pallet_assets::Config>::Balance: From<i32>
 		{
 			let sender = ensure_signed(origin)?;
-			let assetid: AssetId<T> = 1u32.into();
+			let assetid: AssetId<T> = Self::llm_id();
 			let minted_amount: u64 = <MintedAmount<T>>::get(); // Get the amount of llm minted so far
 			let treasury: T::AccountId = PalletId(*b"py/trsry").into_account();
 			let maxcap: u64 = T::Total_supply::get();
 			let t_balance: u64 =
-				pallet_assets::Pallet::<T>::balance(assetid.into(), &treasury.into())
+				pallet_assets::Pallet::<T>::balance(Self::llm_id().into(), &treasury.into())
 					.try_into()
 					.unwrap_or(0u64);
 			let hardlimit: f64 = 0.9;
@@ -271,12 +347,12 @@ pub mod pallet {
 			//	todo!("Mint using pallet assets");
 
 			Self::mint_tokens(assetid, allow_spend as u64); // mint tokens with pallet assets
-	//		<BalanceToAccount<T>>::insert(&sender, amount);
-			// min_balance minus 500
-			//let assit_id: T::AssetId =   //T::AssetId::decode(&mut
-			// 0.into()).unwrap_or(Default::default()); set assetid to 100
-		//	let test0 = assetid; //T::AssetId//::get(); //T::AssetsWrapper::id(0).ok_or(); // T::AssetId::decode(&mut
-					 // 0).unwrap();//_or(Default::default()); treasury account
+												//		<BalanceToAccount<T>>::insert(&sender, amount);
+												// min_balance minus 500
+												//let assit_id: T::AssetId =   //T::AssetId::decode(&mut
+												// 0.into()).unwrap_or(Default::default()); set assetid to 100
+												//	let test0 = assetid; //T::AssetId//::get(); //T::AssetsWrapper::id(0).ok_or(); //
+												// T::AssetId::decode(&mut 0).unwrap();//_or(Default::default()); treasury account
 
 			Ok(())
 		}
@@ -319,19 +395,18 @@ pub mod pallet {
 		}
 	*/
 	impl<T: Config> Pallet<T> {
-
-		
 		//type Balance = <T as pallet::Config>::Balance;
 		// could do like a OriginFor<SenateGroup> or X(Tech) committee
 		fn create_llm(origin: OriginFor<T>) -> DispatchResult {
 			// create asset with pallet assets
 			//	ensure_signed(origin.clone())?; // bad practise
 
-			let assetid: AssetId<T> = 0u32.into();
-			// check if asset is created
+			let assetid: AssetId<T> = Self::llm_id(); //0u32.into();
+										  // check if asset is created
 
 			let owner: T::AccountId = PalletId(*b"py/trsry").into_account(); // treasury is the owner
-			let challenger_lookup: <T::Lookup as StaticLookup>::Source = T::Lookup::unlookup(owner.clone());
+			let challenger_lookup: <T::Lookup as StaticLookup>::Source =
+				T::Lookup::unlookup(owner.clone());
 			let t_ac2: T::AccountId = PalletId(*b"py/trsry").into_account();
 			let asset_balance: u128 = pallet_assets::Pallet::<T>::balance(assetid.into(), t_ac2)
 				.try_into()
@@ -350,12 +425,13 @@ pub mod pallet {
 				challenger_lookup,
 				true,
 				min_balance,
-			).unwrap_or_default();
+			)
+			.unwrap_or_default();
 			let t_ac: T::AccountId = PalletId(*b"py/trsry").into_account();
 			let my_amount: u64 = min_balance.try_into().unwrap_or(0u64);
-			Event::<T>::LLMCreated(t_ac.clone(),my_amount);
-		//	LLMBalance::<T>::insert::<T::AccountId, T::Balance>(t_ac, my_amount.try_into().unwrap_or_default());
-			// set the asset's meta data
+			Event::<T>::LLMCreated(t_ac.clone(), my_amount);
+			//	LLMBalance::<T>::insert::<T::AccountId, T::Balance>(t_ac,
+			// my_amount.try_into().unwrap_or_default()); set the asset's meta data
 			pallet_assets::Pallet::<T>::force_set_metadata(
 				origin.clone(),
 				assetid.into(),
@@ -363,10 +439,15 @@ pub mod pallet {
 				symbol,
 				decimals,
 				false,
-			).unwrap_or_default();
+			)
+			.unwrap_or_default();
 			Self::mint_tokens(assetid, T::PreMintedAmount::get()); // mint the preminted amount
 			Ok(())
 			// pre mint amount and freeze it
+		}
+		//GET LLM ID
+		fn llm_id() -> AssetId<T> {
+			1u32.into()
 		}
 
 		fn try_mint(block: u64) -> DispatchResult {
@@ -397,20 +478,37 @@ pub mod pallet {
 		}
 
 		// get asset metadata from pallet-assets crate
-	//	fn get_asset_metadata(assetid: AssetId<T>) -> Result<(Vec<u8>, Vec<u8>, u8), &'static str> {
-	//			todo!("get_asset_metadata");
-	//			let asset_metadata = pallet_assets::Pallet::<T>::get_metadata(assetid.into());
-	//	}
+		//	fn get_asset_metadata(assetid: AssetId<T>) -> Result<(Vec<u8>, Vec<u8>, u8), &'static
+		// str> { 			todo!("get_asset_metadata");
+		//			let asset_metadata = pallet_assets::Pallet::<T>::get_metadata(assetid.into());
+		//	}
 
-	//	fn get_asset_meta_data(asset_id: AssetId<T>) -> AssetMetaData<T> {
-	//		let asset_meta_data = <AssetMetaData<T>>::get(asset_id);
-	//		todo!("get_asset_meta_data");
-	//	}
+		//	fn get_asset_meta_data(asset_id: AssetId<T>) -> AssetMetaData<T> {
+		//		let asset_meta_data = <AssetMetaData<T>>::get(asset_id);
+		//		todo!("get_asset_meta_data");
+		//	}
 
-		/// amount of blocks until date 
+		/// amount of blocks until date
 		fn timestamp(blockid: u64) -> u64 {
 			//let block_date = <system::Module<T>>::block_number_to_date(blockid);
 			todo!("timestamp");
+		}
+
+		/// used for testing, DO NOT USE IN PRODUCTION
+		fn load_alice(amount: u128, user: T::AccountId) {
+			//let alice: T::AccountId = T::AccountId::from_raw([0u8; 32].into());
+
+			//change balance of alice
+			//	LLMBalance::<T>::try_mutate(account, |balance| {
+			//		*balance = amount.try_into().unwrap_or(0u64);
+			//		Ok(())
+			//	  });
+
+			// add check if alice exist
+			LLMBalance::<T>::insert::<T::AccountId, T::Balance>(
+				user,
+				amount.try_into().unwrap_or_default(),
+			);
 		}
 
 		/// Mint tokens to the treasury account.
@@ -418,13 +516,16 @@ pub mod pallet {
 			let transfer_amount: T::Balance = amount.try_into().unwrap_or(Default::default());
 			let treasury: T::AccountId = PalletId(*b"py/trsry").into_account();
 			// update balance of the treasury account, balances should be u128 and not u64
-			LLMBalance::<T>::insert::<T::AccountId, T::Balance>(treasury.clone(), amount.try_into().unwrap_or_default());
+			LLMBalance::<T>::insert::<T::AccountId, T::Balance>(
+				treasury.clone(),
+				amount.try_into().unwrap_or_default(),
+			);
 			// add the amount that we have minted into MintedAmount to add allow_sped
 			<MintedAmount<T>>::mutate(|minted_amount| *minted_amount += amount);
-			
+
 			pallet_assets::Pallet::<T>::mint_into(assetid.into(), &treasury, transfer_amount)
 				.unwrap_or_default();
-		//	Event::<T>::MintedLLM(treasury.into(), amount); // emit event
+			//	Event::<T>::MintedLLM(treasury.into(), amount); // emit event
 		}
 	}
 }
