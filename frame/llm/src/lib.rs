2d3d6a4c8a44cff484a78ad24d42058d47caf5ce
Kacper Żuk <kacper.zuk@neti-soft.com>
2022-12-08 13:10:57 +0100

    Remove freeze_llm and unused storages from LLM pallet

--- frame/llm/src/lib.rs
+++ frame/llm/src/lib.rs
@@ -41,16 +41,6 @@ pub mod pallet {
 	pub(super) type LLMPolitics<T: Config> =
 		StorageMap<_, Blake2_128Concat, T::AccountId, T::Balance, ValueQuery>;
 
-	#[pallet::storage] // LLM that are frozen in the politics queue
-	#[pallet::getter(fn get_politics_lock)]
-	pub(super) type LLMPoliticsLock<T: Config> =
-		StorageMap<_, Blake2_128Concat, T::AccountId, T::Balance, ValueQuery>;
-
-	#[pallet::storage]
-	#[pallet::getter(fn get_locked_llm)] //locked llm used for voting
-	pub(super) type LockedLLM<T: Config> =
-		StorageMap<_, Blake2_128Concat, T::AccountId, T::Balance, ValueQuery>;
-
 	#[pallet::storage]
 	#[pallet::getter(fn get_fluf)] //locked llm used for voting
 	pub(super) type Withdrawlock<T: Config> =
@@ -316,41 +306,6 @@ pub mod pallet {
 			Self::substract_politi_pooled_stats(ten_percent.try_into().unwrap_or(0u64));
 
 			Self::deposit_event(Event::<T>::LLMPoliticsUnlocked(sender, ten_percent.try_into().unwrap_or_default()));
-			Ok(())
-		}
-
-		/// Freeze X amount of LLM for a certain account
-		#[pallet::weight(10_000)] //change me
-		pub fn freeze_llm(
-			origin: OriginFor<T>,
-			account: T::AccountId,
-			amount: T::Balance,
-		) -> DispatchResult {
-			let sender = ensure_signed(origin.clone())?;
-			// ensure that we have balance
-			ensure!(LLMBalance::<T>::get(&sender) >= amount, Error::<T>::InvalidAmount);
-			let sender_balance: T::Balance = LLMBalance::<T>::get(&sender) - amount;
-			//check if LockedLLM contains the sender account
-			//todo: replace with match statement
-
-			if LockedLLM::<T>::contains_key::<T::AccountId>(sender.clone()) {
-				LockedLLM::<T>::mutate_exists(&sender, |b| {
-					*b = Some(amount + LockedLLM::<T>::get(&sender))
-				}); // dont overwrite it, append to balance
-			} else {
-				LockedLLM::<T>::insert::<T::AccountId, T::Balance>(account.clone(), amount); // lock in the amount
-			}
-
-			pallet_assets::Pallet::<T>::transfer(
-				origin.clone(),
-				Self::llm_id().into(),
-				T::Lookup::unlookup(Self::get_llm_account()),
-				amount.clone(),
-			)
-			.unwrap_or_default();
-
-			LLMBalance::<T>::mutate_exists(&sender, |b| *b = Some(sender_balance));
-
 			Ok(())
 		}
 
