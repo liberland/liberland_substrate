ff83980e5a3b4e7c66d6884edf26c1929c6699bc
Kacper Żuk <kacper.zuk@neti-soft.com>
2023-03-07 12:52:16 +0100

    ensure_nfts_allowed -> ensure_land_nfts_allowed

--- frame/llm/src/lib.rs
+++ frame/llm/src/lib.rs
@@ -728,7 +728,7 @@ pub mod pallet {
 			Ok(())
 		}
 
-		fn ensure_nfts_allowed(account: &T::AccountId) -> Result<(), DispatchError> {
+		fn ensure_land_nfts_allowed(account: &T::AccountId) -> Result<(), DispatchError> {
 			ensure!(Self::is_known_good(account), Error::<T>::NonCitizen);
 			ensure!(Self::check_pooled_llm(account), Error::<T>::NoPolLLM);
 			Ok(())
