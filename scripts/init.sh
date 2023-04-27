831c1ec47c05bae6fd6ffe9cb6d75a06befc3bdf
Kacper Żuk <kacper.zuk@neti-soft.com>
2023-04-27 11:33:31 +0200

    Update docs to use older rust nightly

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,5 @@
 rustup update
 rustup default stable
-rustup update nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup update nightly-2023-01-01
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-01-01
 rustup override set stable
