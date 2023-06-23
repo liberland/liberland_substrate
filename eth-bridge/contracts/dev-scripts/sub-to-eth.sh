c6c02bd35fc116a19aa5d0a1480706564edf2b3b
Kacper Żuk <kacper.zuk@neti-soft.com>
2023-06-23 16:51:36 +0200

    Fix bridge dev-scripts

--- eth-bridge/contracts/dev-scripts/sub-to-eth.sh
+++ eth-bridge/contracts/dev-scripts/sub-to-eth.sh
@@ -2,4 +2,4 @@
 
 set -exuo pipefail
 
-polkadot-js-api --seed "//Alice" tx.llmBridge.deposit 1000000000000 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
+polkadot-js-api --seed "//Alice" tx.ethLLMBridge.deposit 1000000000000 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266
