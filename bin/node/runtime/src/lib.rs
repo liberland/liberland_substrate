c8f28ee82660fea5ae479985b3d60a3d95b3783f
Kacper Żuk <kacper.zuk@neti-soft.com>
2023-09-18 15:34:51 +0200

    BLOCKCHAIN-160 - Allow public referendums for amending treaties

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -1445,7 +1445,10 @@ impl pallet_liberland_legislation::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type Citizenship = LLM;
 	type ConstitutionOrigin = pallet_democracy::EnsureReferendumProportionAtLeast<Self, 3, 4>;
-	type InternationalTreatyOrigin = pallet_collective::EnsureProportionAtLeast<AccountId, CouncilCollective, 1, 2>; // FIXME what ratio?
+	type InternationalTreatyOrigin = EitherOf<
+	 	EnsureRoot<AccountId>,
+		pallet_collective::EnsureProportionAtLeast<AccountId, CouncilCollective, 1, 2>,
+	>;
 	type LowTierDeleteOrigin = EitherOf<
 		EnsureRoot<AccountId>,
 		HalfSenateOrigin
