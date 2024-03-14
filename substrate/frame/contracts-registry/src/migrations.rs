7bc49f2c6d42be2eed955f1837163bd17b7bb59d
Michal Lustyk <MichalLustykNeti>
2024-03-14 15:57:03 +0100

    fixed comments

--- substrate/frame/contracts-registry/src/migrations.rs
+++ substrate/frame/contracts-registry/src/migrations.rs
@@ -1,28 +1,39 @@
 use super::{pallet::Config, *};
 use frame_support::{pallet_prelude::*, storage_alias, traits::OnRuntimeUpgrade};
-use frame_system::pallet_prelude::BlockNumberFor;
 use sp_std::vec::Vec;
 
 #[cfg(feature = "try-runtime")]
 use sp_runtime::TryRuntimeError;
 
-pub mod v2 {
+pub mod v1 {
 	use super::*;
 
-	const TARGET: &'static str = "runtime::llm::migration::v2";
-    pub type I = ();
+	pub type I = ();
+	pub struct Migration<T>(sp_std::marker::PhantomData<T>);
 
-	pub struct Migration<T, OldInterval>(sp_std::marker::PhantomData<(T, OldInterval)>);
+	#[derive(Clone, Encode, Decode, MaxEncodedLen, RuntimeDebug, TypeInfo)]
+	#[scale_info(skip_type_params(MaxContractsLen, MaxParties))]
+	pub struct ContractDataStorage<
+		MaxContractsLen: Get<u32>,
+		MaxParties: Get<u32>,
+		AccountId,
+		Balance,
+	> {
+		pub data: BoundedVec<u8, MaxContractsLen>,
+		pub parties: BoundedVec<AccountId, MaxParties>,
+		pub creator: AccountId,
+		pub deposit: Balance,
+	}
 
-    #[storage_alias]
-    pub type PartiesSignatures<T: Config<I>> = StorageMap<
-        Pallet<T>,
-        Twox64Concat,
-        ContractIndex,
-        Vec<<T as frame_system::Config>::AccountId>,
-        OptionQuery,
-    >;
-	
+	#[storage_alias]
+	pub type PartiesSignatures<T: Config<I>> = StorageMap<
+		Pallet<T>,
+		Twox64Concat,
+		ContractIndex,
+		Vec<<T as frame_system::Config>::AccountId>,
+		OptionQuery,
+	>;
+
 	#[storage_alias]
 	pub type JudgesSignatures<T: Config<I>> = StorageMap<
 		Pallet<T>,
@@ -32,13 +43,36 @@ pub mod v2 {
 		OptionQuery,
 	>;
 
-	impl<T: Config, OldInterval: Get<BlockNumberFor<T>>> OnRuntimeUpgrade
-		for Migration<T, OldInterval>
+	#[storage_alias]
+	pub type Contracts<T: Config<I>, I: 'static = ()> = StorageMap<
+		Pallet<T>,
+		Twox64Concat,
+		ContractIndex,
+		ContractDataStorage<<T as Config>::MaxContractContentLen, <T as Config>::MaxParties, <T as frame_system::Config>::AccountId, BalanceOf<T, I>>,
+		OptionQuery,
+	>;
+}
+
+pub mod v2 {
+	use super::*;
+
+	const TARGET: &'static str = "runtime::contracts-registry::migration::v2";
+    pub type I = ();
+
+	pub struct Migration<T>(sp_std::marker::PhantomData<T>);
+
+	impl<T: Config> OnRuntimeUpgrade
+		for Migration<T>
 	{
 		#[cfg(feature = "try-runtime")]
 		fn pre_upgrade() -> Result<Vec<u8>, TryRuntimeError> {
 			assert!(StorageVersion::get::<Pallet<T>>() == 1, "can only upgrade from version 1");
-
+			let contracts_size = v1::Contracts::<T, I>::iter().count();
+			log::warn!(
+				target: TARGET,
+				"Pre upgrade contracts size: {}",
+				contracts_size
+			);
 			Ok(().encode())
 		}
 
@@ -53,26 +87,24 @@ pub mod v2 {
 				return weight;
 			}
 
-			let _ = v2::JudgesSignatures::<T>::clear(u32::MAX, None);
-			let _ = v2::PartiesSignatures::<T>::clear(u32::MAX, None);
+			let _ = v1::JudgesSignatures::<T>::clear(u32::MAX, None);
+			let _ = v1::PartiesSignatures::<T>::clear(u32::MAX, None);
 
-            Contracts::<T>::translate(|_, maybe_contract_data: Option<
-                    OldContractDataStorage<
+            Contracts::<T>::translate(|_, contract_data: 
+				v1::ContractDataStorage<
                         <T as Config<I>>::MaxContractContentLen, 
                         <T as Config<I>>::MaxParties, 
                         <T as frame_system::Config>::AccountId, 
                         BalanceOf<T, I>
-                    >
                 >| {
-                if let Some(contract_data) = maybe_contract_data {
-                    return Some(ContractDataStorage {
-                         data: contract_data.data,
-                         parties: Some(contract_data.parties),
-                         creator: contract_data.creator,
-                         deposit: contract_data.deposit,
-                    })
-                }
-                None
+              
+				Some(ContractDataStorage {
+						data: contract_data.data,
+						parties: Some(contract_data.parties),
+						creator: contract_data.creator,
+						deposit: contract_data.deposit,
+				})
+
             });
             
             StorageVersion::new(2).put::<Pallet<T>>();
@@ -82,17 +114,14 @@ pub mod v2 {
 		#[cfg(feature = "try-runtime")]
 		fn post_upgrade(_state: Vec<u8>) -> Result<(), TryRuntimeError> {
 			assert_eq!(StorageVersion::get::<Pallet<T>>(), 2, "must upgrade");
+			let contracts_size = Contracts::<T>::iter().count();
+			log::warn!(
+				target: TARGET,
+				"Post upgrade contracts size: {}",
+				contracts_size
+			);
 			Ok(())
 		}
 	}
 }
 
-#[derive(Clone, Encode, Decode, MaxEncodedLen, RuntimeDebug, TypeInfo)]
-#[scale_info(skip_type_params(MaxContractsLen, MaxParties))]
-pub struct OldContractDataStorage<MaxContractsLen: Get<u32>, MaxParties: Get<u32>, AccountId, Balance>
-{
-	pub data: BoundedVec<u8, MaxContractsLen>,
-	pub parties: BoundedVec<AccountId, MaxParties>,
-	pub creator: AccountId,
-	pub deposit: Balance,
-}
