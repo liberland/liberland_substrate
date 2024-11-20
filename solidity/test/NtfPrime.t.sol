fc70cc70e8158ac39c1e150b167ac75a1b41a904
jindra12.underdark <jindra12.underdark@gmail.com>
2024-11-20 23:07:23 +0100

    BLOCKCHAIN-547 Initialize forge project

---
+++ solidity/test/NtfPrime.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {NftPrime} from "../src/NftPrime.sol";
+
+contract NtfPrimeTest is Test {
+    NftPrime public nftPrime;
+
+    function setUp() public {
+        
+    }
+}
