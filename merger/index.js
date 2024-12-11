65c0ea9e5af2a62f12f6a7e81ae397e25778c603
jindra12.underdark <jindra12.underdark@gmail.com>
2024-12-10 20:54:51 -0600

    BLOCKCHAIN-552 start building migration script

---
+++ merger/index.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const { execSync } = require('child_process');
+const { argv } = require("process");
+const { sync } = require("glob");
+
+const mergePaths = {
+    // "our": "their"
+    "substrate/bin/node/cli": "substrate/bin/node/cli",
+    "substrate/bin/node/rpc": "substrate/bin/node/rpc",
+    "substrate/bin/node/runtime": "substrate/bin/node/runtime",
+    "substrate/frame/democracy": "substrate/frame/democracy",
+    "substrate/frame/elections-phragmen": "substrate/frame/elections-phragmen",
+    "substrate/frame/identity": "substrate/frame/identity",
+    "substrate/frame/nfts": "substrate/frame/nfts",
+    "substrate/frame/nfts/runtime-api": "substrate/frame/nfts/runtime-api",
+    "substrate/frame/staking": "substrate/frame/staking",
+    "substrate/frame/staking/reward-curve": "substrate/frame/staking/reward-curve",
+    "substrate/frame/staking/reward-fn": "substrate/frame/staking/reward-fn",
+};
+
+const startup = [
+    "cd ./polkadot-sdk",
+    "git fetch origin",
+    `git checkout ${argv[2]}`,
+];
+
+execSync(startup.join("; "), {
+    stdio: 'inherit',
+});
+
+Object.entries(mergePaths).forEach(([our, their]) => {
+    const theirs = sync(`./polkadot-sdk/${their}/**/*`);
+    console.log(theirs);
+});
