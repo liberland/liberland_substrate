cffa6ee7a0eb0daf12f025c07daa1fc0d04704ba
jindra12.underdark <jindra12.underdark@gmail.com>
2024-12-11 10:56:12 -0600

    BLOCKCHAIN-552 Create merge-type script

--- merger/index.js
+++ merger/index.js
@@ -1,34 +1,42 @@
-const fs = require("fs");
-const { execSync } = require('child_process');
-const { argv } = require("process");
+const { execSync: execSyncInternal } = require('child_process');
 const { sync } = require("glob");
+const { commitId, newBranch, currentBranch, lastMergeCommit } = require("./config.json");
 
-const mergePaths = {
-    // "our": "their"
-    "substrate/bin/node/cli": "substrate/bin/node/cli",
-    "substrate/bin/node/rpc": "substrate/bin/node/rpc",
-    "substrate/bin/node/runtime": "substrate/bin/node/runtime",
-    "substrate/frame/democracy": "substrate/frame/democracy",
-    "substrate/frame/elections-phragmen": "substrate/frame/elections-phragmen",
-    "substrate/frame/identity": "substrate/frame/identity",
-    "substrate/frame/nfts": "substrate/frame/nfts",
-    "substrate/frame/nfts/runtime-api": "substrate/frame/nfts/runtime-api",
-    "substrate/frame/staking": "substrate/frame/staking",
-    "substrate/frame/staking/reward-curve": "substrate/frame/staking/reward-curve",
-    "substrate/frame/staking/reward-fn": "substrate/frame/staking/reward-fn",
-};
-
-const startup = [
-    "cd ./polkadot-sdk",
-    "git fetch origin",
-    `git checkout ${argv[2]}`,
-];
-
-execSync(startup.join("; "), {
+const execSync = (command) => execSyncInternal(command, {
     stdio: 'inherit',
 });
 
-Object.entries(mergePaths).forEach(([our, their]) => {
-    const theirs = sync(`./polkadot-sdk/${their}/**/*`);
-    console.log(theirs);
-});
+const mergePaths = [
+    "substrate/bin/node/cli",
+    "substrate/bin/node/rpc",
+    "substrate/bin/node/runtime",
+    "substrate/frame/democracy",
+    "substrate/frame/elections-phragmen",
+    "substrate/frame/identity",
+    "substrate/frame/nfts",
+    "substrate/frame/nfts/runtime-api",
+    "substrate/frame/staking",
+    "substrate/frame/staking/reward-curve",
+    "substrate/frame/staking/reward-fn",
+];
+
+const startup = [
+    "git remote add polkadot-sdk-upstream https://github.com/paritytech/polkadot-sdk.git",
+    "git remote update",
+    "cd ./polkadot-sdk",
+    "git fetch origin",
+    `git checkout ${commitId}`,
+];
+
+execSync(startup.join("; "));
+
+const theirs = mergePaths.reduce((theirs, their) => {
+    theirs.push(...sync(`${__dirname}/polkadot-sdk/${their}/**/*`));
+    return theirs;
+}, []).map((their) => their.replace(`${__dirname}/polkadot-sdk/`, ""));
+
+execSync(`git checkout ${lastMergeCommit}; git switch --create features/merge-from-${newBranch}`);
+
+execSync(`git checkout polkadot-sdk-upstream/${newBranch} ${theirs.join(' ')}`);
+
+execSync(`git switch ${currentBranch}; git merge features/merge-from-${newBranch}`);
